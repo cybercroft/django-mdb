2d55fca49fa9abe87809aa9d92aea9c5a00c1da6
Eleftherios Daskalakis <leftetris@yahoo.gr>
2024-11-16 21:10:30 +0200

    Registered Product to admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,15 @@
+# inventory/admin.py
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)  # Registering Product model using the decorator
+class ProductAdmin(admin.ModelAdmin):
+    # Define which fields to display in the admin list view
+    list_display = ('name', 'price', 'stock', 'category')
+    # Define which fields to make searchable in the admin
+    search_fields = ('name', 'category')
+    # Define filter options for the admin
+    list_filter = ('category',)
+    # Add additional customizations if necessary (e.g., fieldsets, ordering)
+    ordering = ('name',)
